2d5f2facd00e066f41eff7ea76c91b6343ea3b62
M Ramadhani Febriadi <ramadhani55@gmail.com>
2018-04-03 00:35:51 +0800

    Initial commit

---
+++ src/praktek02fix/Praktek02fix.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek02fix;
+
+/**
+ *
+ * @author Asus x441BA
+ */
+public class Praktek02fix {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
