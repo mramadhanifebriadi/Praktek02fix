5caca812c14d9a87788ee6ea75d3486dfef15a0a
M Ramadhani Febriadi <ramadhani55@gmail.com>
2018-04-03 00:52:13 +0800

    instance objek

--- src/praktek02fix/jajargenjangAksi.java
+++ src/praktek02fix/jajargenjangAksi.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek02fix;
-
-/**
- *
- * @author Asus x441BA
- */
 public class jajargenjangAksi {
+    public static void main(String[] args) {
+        
+        jajargenjang j1 = new jajargenjang();
+    }
     
 }
